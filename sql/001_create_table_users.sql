77f2e85910d1cfae2cf3590d18bd618208f4ad77
NickWallace2001 <ncw6@njit.edu>
2022-02-22 13:02:15 -0500

    user table file

---
+++ sql/001_create_table_users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS 'Users' (
+	'id' INT NOT NULL AUTO_INCREMENT
+	,'email' VARCHAR(100) NOT NULL
+	,'username' VARCHAR(60) NOT NULL
+	,'password' VARCHAR(100) NOT NULL
+	,'created' TIMESTAMP NOT NULL DEFUALT CURRENT_TIMESTAMP
+	,'friend_code' INT(6) NOT NULL
+	,PRIMARY KEY ('id')
+	,UNIQUE ('email')
+	,UNIQUE ('username')
+	,UNIQUE ('friend_code')
+)
