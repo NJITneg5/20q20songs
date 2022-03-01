bbb669cd09eed2f13fda46e4cae4b0e8b737d162
NickWallace2001 <ncw6@njit.edu>
2022-03-01 12:44:20 -0500

    updating rabbit files with functions

--- rabbitMQ/testRabbitMQClient.php
+++ rabbitMQ/testRabbitMQClient.php
@@ -14,17 +14,24 @@ else
   $msg = "This is a test message from Nick or Anthony going through Bryon's Queue to Nate's Server.";
 }
 
-$request = array();
-$request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
-$request['message'] = $msg;
-$response = $client->send_request($request);
-//$response = $client->publish($request);
+function login($username, $password, $username){
+	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
-echo " Group client received response: ".PHP_EOL;
-print_r($response);
-echo "\n\n";
+	$request = array();
+
+	$request['type'] = "login";
+	$request['username'] = $username;
+	$request['password'] = $password;
+	$request['email'] = $email;
+
+	$response = $client->send_request($request);
+	//$response = $client->publish($request);
+
+	return $response;
+}
+//echo " Group client received response: ".PHP_EOL;
+//print_r($response);
+//echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
 
--- rabbitMQ/testRabbitMQServer.php
+++ rabbitMQ/testRabbitMQServer.php
@@ -4,12 +4,42 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function doLogin($username,$password)
+function doLogin($username,$password,$email)
 {
-    // lookup username in databas
-    // check password
-    return true;
-    //return false if not valid
+	global $mydb
+
+	$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+	$query = "select password from Users where email='$email';";
+	$preResult= $mydb->query($query)
+	$result= mysqli_fetch_array($preResult, MYSQLI_ASSOC);
+	$finalResult= $result['password'];
+
+	//check if result returns anything
+	if ($preresult->num_rows == 0){
+		echo "Null Result\n";
+		return false;
+	}
+
+	//validate user info
+	if ($finalResult == $password){
+		echo "Successful Reseult\n";
+		return true;
+	}
+
+	//return false in all other instances
+	else{
+		echo "Failed Result\n";
+		return false
+	}
+}
+
+function doRegistration($email, $username, $password){
+	global $mydb;
+
+	$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+
 }
 
 function requestProcessor($request)
@@ -23,7 +53,7 @@ function requestProcessor($request)
   switch ($request['type'])
   {
     case "login":
-      return doLogin($request['username'],$request['password']);
+      return doLogin($request['username'],$request['password'],$request['email'		]);
     case "validate_session":
       return doValidate($request['sessionId']);
   }
@@ -36,5 +66,7 @@ echo "GroupTestRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
 echo "GroupTestRabbitMQServer END".PHP_EOL;
 exit();
+
+$mydb= new mysqli('localhost','testUser','12345','testdb');
 ?>
 
