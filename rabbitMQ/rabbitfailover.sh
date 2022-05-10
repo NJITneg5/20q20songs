b20d9a0ef74ac7576537506422b931aad1046b03
NickWallace2001 <ncw6@njit.edu>
2022-05-10 08:33:42 -0400

    last minute

---
+++ rabbitMQ/rabbitfailover.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+host=$1
+declare -i flag=1
+declare -i check=1
+function pingcheck
+{
+	ping=`ping -c 1 $host | grep bytes | wc -l`
+	if [ "$ping" -gt 1 ];then
+		check=1
+	else
+		check=0	
+	fi
+}
+
+while [ $flag == 1 ]
+do
+	pingcheck
+	echo "main system still running"
+	if [ $check == 0 ];then
+		scp ../webServer/partials/testRabbitMQ.ini it490@172.24.205.67:/home/it490/git/20q20songs/webServer/partials
+                scp ../webServer/partials/nickLoggingRabbitMQ.ini it490@172.24.205.67:/home/it490/git/20q20songs/webServer/partials
+		#scp ../webServer/partials/testRabbitMQ.ini
+		#scp ../webServer/partials/nateLoggingRabbitMQ.ini
+		scp ../webServer/partials/testRabbitMQ.ini anthony@172.24.187.221:/home/anthony/Desktop/20q20songs-working/webServer/partials
+		scp ../webServer/partials/anthonyLoggingRabbitMQ.ini anthony@172.24.187.221:/home/anthony/Desktop/20q20songs-working/webServer/partials	
+		flag=0
+		
+	else
+		sleep 30
+		continue
+	fi
+	
+done
---
+++ rabbitMQ/restoreini.sh
@@ -0,0 +1,8 @@
+#!/bin/bsh
+
+scp ../webServer/partials/restore/testRabbitMQ.ini it490@172.24.205.67:/home/it490/git/20q20songs/webServer/partials
+scp ../webServer/partials/restore/nickLoggingRabbitMQ.ini it490@172.24.205.67:/home/it490/git/20q20songs/webServer/partials
+#scp ../webServer/partials/restore/testRabbitMQ.ini
+#scp ../webServer/partials/restore/nateLoggingRabbitMQ.ini
+scp ../webServer/partials/restore/testRabbitMQ.ini anthony@172.24.187.221:/home/anthony/Desktop/20q20songs-working/webServer/partials
+scp ../webServer/partials/restore/anthonyLoggingRabbitMQ.ini anthony@172.24.187.221:/home/anthony/Desktop/20q20songs-working/webServer/partials	
